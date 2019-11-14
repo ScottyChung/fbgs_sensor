8adad2f1c3ed1eb7b424e2b5e84d2ac74c6757d6
root <scottc213@gmail.com>
2019-11-14 19:44:00 +0000

    Added placeholder UI for now

--- src/fbgs_sensing_node.py
+++ src/fbgs_sensing_node.py
@@ -3,6 +3,11 @@ import rospy
 import socket
 import struct
 import sys
+import os
+import rospkg
+from PyQt4 import QtGui, uic
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 from std_msgs.msg import Float32MultiArray, MultiArrayLayout, MultiArrayDimension
 
 def parse_message(message):
@@ -42,55 +47,81 @@ def get_wavelengths(fbgs):
             wavelengths.extend(fbgs[key]['peak_wavelengths'])
     return wavelengths
 
-# Create ros publisher and start node
-pub = rospy.Publisher('fbgs_strain', Float32MultiArray, queue_size=10)
-pub_wavelength = rospy.Publisher('fbgs_wavelength', Float32MultiArray, queue_size=10)
-rospy.init_node('fbgs_sensor', anonymous=True)
+class MyWindow(QtGui.QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        rp = rospkg.RosPack()
+        ui_path = os.path.join(rp.get_path('fbgs_sensor'), 'resources', 'fbgs_sensor.ui')
+        uic.loadUi(ui_path, self)
+        self.connectChk.clicked.connect(self.run)
+        self.show()
 
-# Create TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def run(self):
+        #Handle no host
+        if self.hostInput.text().isEmpty():
+            self.connectChk.setChecked(False)
+            return
+        # Create ros publisher and start node
+        pub = rospy.Publisher('fbgs_strain', Float32MultiArray, queue_size=10)
+        pub_wavelength = rospy.Publisher('fbgs_wavelength', Float32MultiArray, queue_size=10)
+        rospy.init_node('fbgs_sensor', anonymous=True)
 
-# Connect to port where server is listening
-host_computer = '1612-ultron'
-port = 2055
-server_address = (host_computer, port)
+        # Create TCP/IP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print('Connecting to %s:%s' % server_address)
-sock.connect(server_address)
+        # Connect to port where server is listening
+        host_computer = self.hostInput.text().split(':')[0]
+        port = int(self.hostInput.text().split(':')[-1])
+        server_address = (host_computer, port)
 
-while True:
-    try:
-        # Read size of data
-        data_size = sock.recv(4)
-        data_size = struct.unpack(">i",data_size)[0] # Convert bytestring to int32
+        print('Connecting to %s:%s' % server_address)
 
-        # Collect expected amount of data
-        data_message = b''
-        data_rec = 0
-        while data_rec < data_size:
-            new_data = sock.recv(128)
-            data_rec += len(new_data)
-            data_message += new_data
+        try:
+            sock.connect(server_address)
+        except:
+            self.connectChk.setChecked(False)
+            print('Error when connecting. Check host name')
+            return
 
-        if data_message: # New message to publish
-            data = parse_message(data_message)
-            # Create strain message
-            dim = MultiArrayDimension('length', data['strain_count'], 1)
-            layout = MultiArrayLayout([dim], 0)
-            strain_message = Float32MultiArray(layout, data['strain'])
-            pub.publish(strain_message)
-            
-            # Create wavelength message
-            wavelengths = get_wavelengths(data)
-            dim = MultiArrayDimension('length', len(wavelengths), 1)
-            layout = MultiArrayLayout([dim], 0)
-            wavelength_message = Float32MultiArray(layout, wavelengths)
-            pub_wavelength.publish(wavelength_message)
+        while True:
+            try:
+                # Read size of data
+                data_size = sock.recv(4)
+                data_size = struct.unpack(">i",data_size)[0] # Convert bytestring to int32
+
+                # Collect expected amount of data
+                data_message = b''
+                data_rec = 0
+                while data_rec < data_size:
+                    new_data = sock.recv(128)
+                    data_rec += len(new_data)
+                    data_message += new_data
+
+                if data_message: # New message to publish
+                    data = parse_message(data_message)
+                    # Create strain message
+                    dim = MultiArrayDimension('length', data['strain_count'], 1)
+                    layout = MultiArrayLayout([dim], 0)
+                    strain_message = Float32MultiArray(layout, data['strain'])
+                    pub.publish(strain_message)
+                    
+                    # Create wavelength message
+                    wavelengths = get_wavelengths(data)
+                    dim = MultiArrayDimension('length', len(wavelengths), 1)
+                    layout = MultiArrayLayout([dim], 0)
+                    wavelength_message = Float32MultiArray(layout, wavelengths)
+                    pub_wavelength.publish(wavelength_message)
 
 
-        #sys.stdout.write('\rMessage received: {0}\r'.format(data_message))
+                #sys.stdout.write('\rMessage received: {0}\r'.format(data_message))
 
-    except KeyboardInterrupt:
-        print('Closing socket')
-        sock.close()
-        sys.exit()
+            except KeyboardInterrupt:
+                print('Closing socket')
+                sock.close()
+                sys.exit()
+
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    window = MyWindow()
+    sys.exit(app.exec_())
