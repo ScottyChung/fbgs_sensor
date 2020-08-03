571b5552511284ddfd3aa62a1eb43f92f7340328
Scotty <scottc213@gmail.com>
2020-08-03 14:49:33 -0400

    Updated formatting and added more comments

--- src/fbgs_sensing_node.py
+++ src/fbgs_sensing_node.py
@@ -10,61 +10,72 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from std_msgs.msg import Float32MultiArray, MultiArrayLayout, MultiArrayDimension
 
+
 def recv_msg(sock):
     # Read size of data
-    data_size = recv_all(sock,4)
+    data_size = recv_all(sock, 4)
     if not data_size:
         return None
-    data_size = struct.unpack(">i",data_size)[0] # Convert bytestring to int32
+    data_size = struct.unpack(">i", data_size)[0]  # Convert bytestring to int32
     return recv_all(sock, data_size)
 
+
 def recv_all(sock, n):
-    #Helper to grab a spefic amount of bytes
+    # Helper to grab a spefic amount of bytes
     data = bytearray()
     while len(data) < n:
-        packet = sock.recv(n-len(data))
+        packet = sock.recv(n - len(data))
         if not packet:
             return None
         data.extend(packet)
     return data
-    
+
+
 def parse_message(message):
     data_message = message.split('\t')
     # Reverse message to pop off easier
     dm = data_message[::-1]
-    data = {}
-    data['date'] = dm.pop()
-    data['time'] = dm.pop()
-    data['count'] = int(dm.pop())
-    data['measured_channels'] = int(dm.pop())
-   
-    for i in xrange(data['measured_channels']):
-        channel = {}
-        channel['name'] = int(dm.pop())
-        channel['sensor_count'] = int(dm.pop())
-        channel['error'] = dm[-4:]
-        dm=dm[0:-4]
-        channel['peak_wavelengths'] = dm[-channel['sensor_count']:]; dm = dm[0:-channel['sensor_count']]
-        channel['peak_wavelengths'] = [float(w) for w in channel['peak_wavelengths']] # Cast to float
-        channel['peak_wavelengths'].reverse()   # maintain order
-        channel['peak_powers'] = dm[-channel['sensor_count']:]; dm = dm[0:-channel['sensor_count']]
-        channel['peak_powers'] = [float(p) for p in channel['peak_powers']] # Cast to float
-        channel['peak_powers'].reverse()        # maintain order
+    data = {'date': dm.pop(),
+            'time': dm.pop(),
+            'count': int(dm.pop()),
+            'measured_channels': int(dm.pop())}
+
+    for i in range(data['measured_channels']):
+        # Header information
+        channel = {'name': int(dm.pop()),
+                   'sensor_count': int(dm.pop()),
+                   'error': dm[-4:]}
+        dm = dm[0:-4]
+
+        # Getting wavelengths
+        channel['peak_wavelengths'] = dm[-channel['sensor_count']:]
+        dm = dm[0:-channel['sensor_count']]
+        channel['peak_wavelengths'] = [float(w) for w in channel['peak_wavelengths']]  # Cast to float
+        channel['peak_wavelengths'].reverse()  # maintain order
+
+        # Get peak power
+        channel['peak_powers'] = dm[-channel['sensor_count']:]
+        dm = dm[0:-channel['sensor_count']]
+        channel['peak_powers'] = [float(p) for p in channel['peak_powers']]  # Cast to float
+        channel['peak_powers'].reverse()  # maintain order
 
         # Nest channels inside of data structure
         data['channel_{0}'.format(channel['name'])] = channel
 
+    # Get strain values
     data['strain_count'] = int(dm.pop())
     data['strain'] = [float(d.split('\r')[0]) for d in dm[-data['strain_count']:]]
-    data['strain'].reverse()    # maintain order
+    data['strain'].reverse()  # maintain order
     return data
+
 
 def get_wavelengths(fbgs):
     wavelengths = []
-    for key, value in sorted(fbgs.iteritems()):
+    for key, value in sorted(fbgs.items()):
         if key.startswith('channel'):
             wavelengths.extend(fbgs[key]['peak_wavelengths'])
     return wavelengths
+
 
 class MyWindow(QtGui.QMainWindow):
     def __init__(self):
@@ -76,13 +87,16 @@ class MyWindow(QtGui.QMainWindow):
         self.show()
 
     def run(self):
-        #Handle no host
+        # Handle no host
         if self.hostInput.text().isEmpty():
             self.connectChk.setChecked(False)
             return
-        # Create ros publisher and start node
+
+        # Adding publishers
         pub = rospy.Publisher('fbgs_strain', Float32MultiArray, queue_size=10)
         pub_wavelength = rospy.Publisher('fbgs_wavelength', Float32MultiArray, queue_size=10)
+
+        # Starting node
         rospy.init_node('fbgs_sensor', anonymous=True)
 
         # Create TCP/IP socket
@@ -107,23 +121,21 @@ class MyWindow(QtGui.QMainWindow):
             try:
                 data_message = recv_msg(sock)
 
-                if data_message: # New message to publish
+                if data_message:  # New message to publish
                     data = parse_message(data_message)
+
                     # Create strain message
                     dim = MultiArrayDimension('length', data['strain_count'], 1)
                     layout = MultiArrayLayout([dim], 0)
                     strain_message = Float32MultiArray(layout, data['strain'])
                     pub.publish(strain_message)
-                    
+
                     # Create wavelength message
                     wavelengths = get_wavelengths(data)
                     dim = MultiArrayDimension('length', len(wavelengths), 1)
                     layout = MultiArrayLayout([dim], 0)
                     wavelength_message = Float32MultiArray(layout, wavelengths)
                     pub_wavelength.publish(wavelength_message)
-
-
-                #sys.stdout.write('\rMessage received: {0}\r'.format(data_message))
 
             except KeyboardInterrupt:
                 print('Closing socket')
